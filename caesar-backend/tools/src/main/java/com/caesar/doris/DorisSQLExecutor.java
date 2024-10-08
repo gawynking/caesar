65f6b033318b3bd016c1a7ec1c6f2b7061474d02
chavinking <chavinking1990@gmail.com>
2024-10-09 02:01:17 +0800

    添加工具模块

---
+++ caesar-backend/tools/src/main/java/com/caesar/doris/DorisSQLExecutor.java
@@ -0,0 +1,29 @@
+package com.caesar.doris;
+
+import com.caesar.AbstractSqlExecutor;
+
+import java.sql.Connection;
+
+/**
+ * Doris SQL提交入口
+ */
+public class DorisSQLExecutor extends AbstractSqlExecutor<Connection> {
+
+    public static void main(String[] args) {
+    }
+
+    @Override
+    protected Connection openConnect() {
+        return null;
+    }
+
+    @Override
+    protected void executeSQL(String sql) {
+
+    }
+
+    @Override
+    protected void closeConnect(Connection connection) {
+
+    }
+}
---
+++ caesar-backend/tools/src/main/java/com/caesar/spark/SparkSQLExecutor.java
@@ -0,0 +1,51 @@
+package com.caesar.spark;
+
+
+import com.caesar.AbstractSqlExecutor;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.logging.Logger;
+
+
+/**
+ * Spark SQL提交入口
+ */
+public class SparkSQLExecutor extends AbstractSqlExecutor<SparkSession> {
+
+    private static final Logger logger = Logger.getLogger(SparkSQLExecutor.class.getName());
+
+
+    public static void main(String[] args) {
+        new SparkSQLExecutor().execute(args, new MetaCallback<SparkSession>() {
+            @Override
+            public void process(SparkSession sparkSession, String sql) throws Exception {
+                sparkSession.sql(sql).printSchema();
+            }
+        });
+    }
+
+
+    @Override
+    protected SparkSession openConnect() {
+        return SparkSession.builder()
+                .appName("Spark SQL Executor Java")
+                .enableHiveSupport()  // 如果需要 Hive 支持
+                .getOrCreate();
+    }
+
+    @Override
+    protected void executeSQL(String sql) {
+        try {
+            t.sql(sql);
+        } catch (Exception e) {
+            logger.warning("Error executing SQL: " + sql + ". Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void closeConnect(SparkSession sparkSession) {
+        sparkSession.close();
+    }
+
+}
